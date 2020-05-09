a486c0f676e4682406db9218b88338f7b5e2b243
Megan Murphy <childovhurin@gmail.com>
2020-05-09 18:42:35 -0400

    recmmitting new repo for eat da burger

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"1 lb Beef Cheese Burger",
+0
+),
+(
+"Vegan Cheese Burger",
+0
+),
+(
+"Chicken Patty Melt Burger",
+0
+);
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import ORM to create functions that interact database
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export database functions for controller
+module.exports = burger;
